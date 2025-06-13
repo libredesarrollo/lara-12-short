f55c37ac9329191810e84f228b84ac6e219286f9
Andres Cruz <desarrollolibre.net@gmail.com>
2025-06-13 06:21:07 -0400

    varios

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>Contact</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint consequuntur tempora blanditiis eligendi explicabo nam quidem provident assumenda mollitia repellendus, error reprehenderit, obcaecati dignissimos repudiandae voluptatem, quos a sequi earum.</p>
+</body>
+</html>
