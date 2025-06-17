a6d023693ef82595ab7982fb4508c15edcfdcf71
Andres Cruz <desarrollolibre.net@gmail.com>
2025-06-17 07:18:37 -0400

    varios

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/contact', function () {
-
-    return view('contact',['name' => 'Andres']);
+// Route::get('/contact', function () {
+//     return view('contact',['name' => 'Andres']);
+// });
+// Route::get('/contact/{name?}', function ($name2 = 'juan') {
+// Route::get('/contact/{name?}', function (?string $name2 = 'juan') {
+Route::get('/contact/{name?}', function (?string $name = null) {
+    // return view('contact',['name' => $name ?? 'hola']);
+    $dbdata = ['asas'];
+    $dbdata2 = ['asas'];
+    // return view('contact',['name' => $name ?? 'hola', 'dbdata' => $dbdata,, 'dbdata2' => $dbdata2]);
+    return view('contact',compact('name', 'dbdata','dbdata2'));
 });
